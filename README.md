939b12d20c3dba596f83aa4bad537e937e8fdc85
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2023-09-19 00:44:02 +0000

    Mais elementos

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # 7-de-setembro
-Uma breve descrição de 7 de setembro
+Uma breve descrição de 7 de setembro, que no caso foi a indepedência do Brasil
--- script.js
+++
@@ -1,6 +0,0 @@
-const imgs = document.querySelectorAll(".foto_mural")
-imgs.forEach((colecao) =>{
-    colecao.addEventListener("click",()=>{
-        colecao.classList.toggle("zoom")
-    })
-})
